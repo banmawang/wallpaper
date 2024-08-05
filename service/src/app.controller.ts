628a0c5584bd0e4c30d91c4103af740ea5401fa4
banmawang <banmawang2021@163.com>
2024-08-05 22:13:47 +0800

    添加 element-plus 图片加载效果

--- service/src/app.controller.ts
+++ service/src/app.controller.ts
@@ -1,23 +1,27 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { readdirSync, readFileSync } from 'fs';
 import { resolve } from 'path';
 import _ from 'lodash';
 import { Response } from 'express';
+import { readdir, readFile } from 'fs/promises';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Res() res: Response) {
-    const files = readdirSync(resolve(__dirname, '..', 'wallpaper'));
+  async getHello(@Res() res: Response) {
+    const files = await readdir(resolve(__dirname, '..', 'wallpaper'));
 
-    const file = readFileSync(
-      resolve(__dirname, '..', 'wallpaper', files[_.random(files.length - 1)]),
+    const file = await readFile(
+      resolve(__dirname, '../wallpaper', files[_.random(files.length - 1)]),
     );
-    res.type('image/jpeg');
-    return res.send(file);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        res.type('image/jpeg');
+        resolve(res.send(file));
+      }, 2000);
+    });
 
     // return (
     //   'http://localhost:3000/wallpaper/' + files[_.random(files.length - 1)]
--- src/renderer/src/views/home.vue
+++ src/renderer/src/views/home.vue
@@ -1,11 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElLoading } from 'element-plus'
+import { ref } from 'vue'
+
+const img = ref<HTMLImageElement>()
+const load = () => {
+  const loading = ElLoading.service({ background: 'rgba(255,255,255,.2)' })
+  img.value!.src = `http://localhost:3000?k${Math.random()}`
+  img.value!.addEventListener('load', () => {
+    loading.close()
+  })
+}
+</script>
 
 <template>
   <main class="">
     <img
+      ref="img"
       src="http://localhost:3000/wallpaper/fiona-PEJHMgx_ilo-unsplash.jpg"
-      class="aspect-video no-drag"
+      class="aspect-video no-drag cursor-pointer"
       draggable="false"
+      @click="load"
     />
     <div
       class="cursor-pointer bg-gray-200 text-center py-3 m-3 rounded-lg hover:bg-gray-300 duration-500 opacity-80 shadow-sm no-drag"
