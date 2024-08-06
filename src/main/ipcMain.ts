8c8a61771dbc8a85a830a8b8b1ae4f4c1565691e
banmawang <banmawang2021@163.com>
2024-08-06 11:30:46 +0800

    完成电脑壁纸设置操作

--- src/main/ipcMain.ts
+++ src/main/ipcMain.ts
@@ -1,6 +1,19 @@
-import { ipcMain, IpcMainEvent } from 'electron'
+import { dialog, ipcMain, IpcMainEvent } from 'electron'
+import { createWriteStream } from 'node:fs'
+import { pipeline } from 'node:stream'
+import { promisify } from 'node:util'
+import fetch from 'node-fetch'
+import { resolve } from 'node:path'
+import wallpaper from 'wallpaper'
 
-ipcMain.on('setWallpaper', (_event: IpcMainEvent, url: string) => {
-  console.log(url)
-  //1图片下载，2设置电脑壁纸
+ipcMain.on('setWallpaper', async (_event: IpcMainEvent, url: string) => {
+  const localFile = resolve(__dirname, '../../wallpaper', url.split('/').pop()!)
+  const streamPipeline = promisify(pipeline)
+  const response = await fetch(url)
+  if (!response.ok) {
+    dialog.showErrorBox('斑马兽温馨提示', '图片下载失败')
+    throw new Error(`unexpected response ${response.statusText}`)
+  }
+  await streamPipeline(response.body!, createWriteStream(localFile))
+  wallpaper.set(localFile, { screen: 'all', scale: 'auto' })
 })
